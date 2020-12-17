0e54b7bf6f0e8612d89bc21de102fb8925d6c5f3
Gaurav Thakur <gthakur581@gmail.com>
2020-12-17 18:28:38 +0530

    Stable Release.

--- pages/[slug].js
+++ pages/[slug].js
@@ -28,8 +28,8 @@ export default function IndexPage({ postDetail, errorCode, posts }) {
       </Head>
       <div className="flex flex-col min-h-screen">
         <Nav />
-        <div className="px-2 md:px-10 lg:px-20 gap-10 flex-1 max-w-5xl mx-auto mt-16 md:mt-20 md:mb-6">
-          <div className="bg-white  my-14  px-3 sm:px-5 md:px-10 py-10 col-span-3 rounded-lg dark:bg-deepDarkGray dark:text-gray-400">
+        <div className="px-2 md:px-10 lg:px-20 gap-10 flex-1 max-w-5xl mx-auto mt-28">
+          <div className="bg-white  mb-14  px-3 sm:px-5 md:px-10 py-10 col-span-3 rounded-lg dark:bg-deepDarkGray dark:text-gray-400">
             <h1 className="title">{postDetail.title}</h1>
             <div className="border-t-2 border-b-2 border-black dark:border-gray-500 dark:text-gray-400">
               <div className="grid grid-cols-3 py-2 max-w-md text-center mx-auto">
